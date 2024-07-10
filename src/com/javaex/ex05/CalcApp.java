047e027869c5949d709cd295beec56efd160702a
aripos1 <ihsjjang@gmail.com>
2024-07-10 18:12:32 +0900

    2024-07-10

---
+++ src/com/javaex/ex05/CalcApp.java
@@ -0,0 +1,21 @@
+package com.javaex.ex05;
+
+import java.util.Scanner;
+
+public class CalcApp {
+
+    public static void main(String[] args) {
+
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print(">>");
+    	int num1 = sc.nextInt();
+    	int num2 = sc.nextInt();
+    	char[] calcArr= new char[5];   	
+   
+    	
+    	
+
+
+    	
+    }
+}
